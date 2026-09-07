af72fcdc67f82bd0c7b07f8f3d90f28c26b4935c
wanshui8 <962676545@qq.com>
2026-09-07 10:19:35 +0800

    修改证书登陆

--- admin_client/main.cpp
+++ admin_client/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
 #include "adminwindow.h"
-int main(int argc,char *argv[]){QApplication app(argc,argv);AdminWindow w;w.show();return app.exec();}
+int main(int argc,char *argv[]){
+    QApplication app(argc,argv);AdminWindow w;w.show();return app.exec();}
 
